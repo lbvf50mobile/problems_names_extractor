0629c2c0bbe63edd286ddc91e430394740320123
Dmitry <lbvf50.mobile@gmail.com>
2019-12-09 19:23:10 +0700

    Get problems from all messages.html files.

--- extractor.rb
+++ extractor.rb
@@ -1,10 +1,11 @@
-filename = "messages.html"
-messages_array = File.readlines(filename)
-p messages_array.size
+files_pattern = /^messages[[:digit:]]*.html$/
+messages_array = Dir.entries(".").select{|x| files_pattern === x}
+.map{|filename| File.readlines(filename)}.flatten
+p "Strings: #{messages_array.size}"
 messages_string = messages_array.join
-p messages_string.size
+p "Characters: #{messages_string.size}"
 problems_urls = messages_string.scan(/https:\/\/leetcode.com\/problems\/[a-z.\/\-0-9]+/).uniq
-p problems_urls.size
+p "Problems: #{problems_urls.size}"
 
 readme_array = ["### List of Leetcode tasks."," "] + problems_urls.map{|x| "- " + x}
 
